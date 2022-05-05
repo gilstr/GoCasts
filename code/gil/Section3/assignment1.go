2e7ae2d59403852ffff38a09b566159f9db99660
gilstrauss <gil@cg.solutions>
2022-05-05 11:46:46 +0300

    Section 3, assignment 1

---
+++ code/gil/Section3/assignment1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := range [10]int{} {
+		fmt.Printf("%v: ", i)
+		if i%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+	}
+}
