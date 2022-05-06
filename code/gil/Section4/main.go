e559b1ac2508e1450b5c76733b66cbe11a96506a
gilstrauss <gil@cg.solutions>
2022-05-06 15:16:57 +0300

    Lesson 40

--- code/gil/Section4/main.go
+++ code/gil/Section4/main.go
@@ -1,1 +1,19 @@
-package Section4
+package main
+
+import "fmt"
+
+type student struct {
+	firstName string
+	lastName  string
+}
+
+func (s student) print() {
+	fmt.Printf("%+v\n", s)
+}
+
+func main() {
+	var s1 student
+	s1.print()
+	s1.firstName = "William"
+	s1.print()
+}
